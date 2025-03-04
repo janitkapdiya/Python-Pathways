cb00be7a2b6fde738956e0cfd0a185b2f25e23b5
Janit Kapadiya <janitkapdiya@gmail.com>
2025-03-04 08:47:16 +0530

    Update 03.rules-of-variables.py

--- 02.Variables-&-Datatypes/03.rules-of-variables.py
+++ 02.Variables-&-Datatypes/03.rules-of-variables.py
@@ -1,6 +1,7 @@
 """
 ============== RULES ==============
-"""2. A variable name can only start with an alphabet and underscores.
+1. A variable name can contain alphabets, digits, and underscores.
+2. A variable name can only start with an alphabet and underscores.
 3. A variable name can't start with a digit.
 4. No while space is allowed to be used inside a variable name.
 """
